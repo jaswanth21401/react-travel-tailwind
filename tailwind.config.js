f1b33b75922ff4c8cf8916b39d3af9f7598bd718
jaswanth21401 <jaswanth21401@gmail.com>
2023-07-27 15:07:04 +0530

    jassu update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}","/home/applines/JASWANTH/REACT/Creek/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
